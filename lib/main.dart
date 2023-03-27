a0aa2133bd9b6d983adcdf33830f5564fa90c880
Eduardo-Henrique-Adams <eduadams2004@gmail.com>
2023-03-27 16:35:42 -0300

    youtube

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:youtube/Home.dart';
+import 'Home.dart';
+
+void main() => runApp(MaterialApp(
+  home:Home(),
+  debugShowCheckedModeBanner: false,
+));
+
